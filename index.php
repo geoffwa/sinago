092b645e713c762dcf982ecf085a18fcd43d90e9
Geoffrey Giesemann <geoffrey.giesemann@rea-group.com>
2012-08-23 11:15:18 +1000

    'hello world' slim app

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require 'Slim/Slim.php';
+
+//With default settings
+$app = new Slim();
+
+//With custom settings
+$app = new Slim(array(
+  'log.enable' => true,
+  'log.path' => './logs',
+  'log.level' => 4,
+ // 'view' => 'MyCustomViewClassName'
+));
+
+//GET route
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+//POST route
+$app->post('/person', function () {
+    //Create new Person
+});
+
+//PUT route
+$app->put('/person/:id', function ($id) {
+    //Update Person identified by $id
+});
+
+//DELETE route
+$app->delete('/person/:id', function ($id) {
+    //Delete Person identified by $id
+});
+
+$app->run();
+
+?>
